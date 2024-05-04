c517bf5b9f10d823fbef544540eb4247d555a3cb
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2024-05-04 11:17:29 +0700

    feat: update matrix4 and transformation

--- src/utils/math/matrix4.ts
+++ src/utils/math/matrix4.ts
@@ -1,3 +1,5 @@
+import { Vector3 } from './vector3.ts';
+
 /**
  * This is a flatten 4x4 matrix
  * with row-major orientation
@@ -197,10 +199,17 @@ export class Matrix4 {
     ]);
   }
 
+  /**
+   * Create an identity 4x4 Matrix
+   */
   public static identity(): Matrix4 {
     return new Matrix4([1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1]);
   }
 
+  /**
+   * Transpose this matrix
+   * Return a new instance of matrix
+   */
   public transpose(): Matrix4 {
     const m = this.elements;
     const dst = [];
@@ -223,5 +232,238 @@ export class Matrix4 {
     dst[15] = m[15];
 
     return new Matrix4(dst);
+  }
+
+  /**
+   * Find the inverse of a matrix
+   *
+   * Return new instance of inversed matrix
+   */
+  public inverse(): Matrix4 {
+    const dst = [];
+    const m = this.elements;
+
+    var m00 = m[0 * 4 + 0];
+    var m01 = m[0 * 4 + 1];
+    var m02 = m[0 * 4 + 2];
+    var m03 = m[0 * 4 + 3];
+    var m10 = m[1 * 4 + 0];
+    var m11 = m[1 * 4 + 1];
+    var m12 = m[1 * 4 + 2];
+    var m13 = m[1 * 4 + 3];
+    var m20 = m[2 * 4 + 0];
+    var m21 = m[2 * 4 + 1];
+    var m22 = m[2 * 4 + 2];
+    var m23 = m[2 * 4 + 3];
+    var m30 = m[3 * 4 + 0];
+    var m31 = m[3 * 4 + 1];
+    var m32 = m[3 * 4 + 2];
+    var m33 = m[3 * 4 + 3];
+    const tmp_0 = m22 * m33;
+    const tmp_1 = m32 * m23;
+    const tmp_2 = m12 * m33;
+    const tmp_3 = m32 * m13;
+    const tmp_4 = m12 * m23;
+    const tmp_5 = m22 * m13;
+    const tmp_6 = m02 * m33;
+    const tmp_7 = m32 * m03;
+    const tmp_8 = m02 * m23;
+    const tmp_9 = m22 * m03;
+    const tmp_10 = m02 * m13;
+    const tmp_11 = m12 * m03;
+    const tmp_12 = m20 * m31;
+    const tmp_13 = m30 * m21;
+    const tmp_14 = m10 * m31;
+    const tmp_15 = m30 * m11;
+    const tmp_16 = m10 * m21;
+    const tmp_17 = m20 * m11;
+    const tmp_18 = m00 * m31;
+    const tmp_19 = m30 * m01;
+    const tmp_20 = m00 * m21;
+    const tmp_21 = m20 * m01;
+    const tmp_22 = m00 * m11;
+    const tmp_23 = m10 * m01;
+
+    const t0 =
+      tmp_0 * m11 +
+      tmp_3 * m21 +
+      tmp_4 * m31 -
+      (tmp_1 * m11 + tmp_2 * m21 + tmp_5 * m31);
+    const t1 =
+      tmp_1 * m01 +
+      tmp_6 * m21 +
+      tmp_9 * m31 -
+      (tmp_0 * m01 + tmp_7 * m21 + tmp_8 * m31);
+    const t2 =
+      tmp_2 * m01 +
+      tmp_7 * m11 +
+      tmp_10 * m31 -
+      (tmp_3 * m01 + tmp_6 * m11 + tmp_11 * m31);
+    const t3 =
+      tmp_5 * m01 +
+      tmp_8 * m11 +
+      tmp_11 * m21 -
+      (tmp_4 * m01 + tmp_9 * m11 + tmp_10 * m21);
+
+    const d = 1.0 / (m00 * t0 + m10 * t1 + m20 * t2 + m30 * t3);
+
+    dst[0] = d * t0;
+    dst[1] = d * t1;
+    dst[2] = d * t2;
+    dst[3] = d * t3;
+    dst[4] =
+      d *
+      (tmp_1 * m10 +
+        tmp_2 * m20 +
+        tmp_5 * m30 -
+        (tmp_0 * m10 + tmp_3 * m20 + tmp_4 * m30));
+    dst[5] =
+      d *
+      (tmp_0 * m00 +
+        tmp_7 * m20 +
+        tmp_8 * m30 -
+        (tmp_1 * m00 + tmp_6 * m20 + tmp_9 * m30));
+    dst[6] =
+      d *
+      (tmp_3 * m00 +
+        tmp_6 * m10 +
+        tmp_11 * m30 -
+        (tmp_2 * m00 + tmp_7 * m10 + tmp_10 * m30));
+    dst[7] =
+      d *
+      (tmp_4 * m00 +
+        tmp_9 * m10 +
+        tmp_10 * m20 -
+        (tmp_5 * m00 + tmp_8 * m10 + tmp_11 * m20));
+    dst[8] =
+      d *
+      (tmp_12 * m13 +
+        tmp_15 * m23 +
+        tmp_16 * m33 -
+        (tmp_13 * m13 + tmp_14 * m23 + tmp_17 * m33));
+    dst[9] =
+      d *
+      (tmp_13 * m03 +
+        tmp_18 * m23 +
+        tmp_21 * m33 -
+        (tmp_12 * m03 + tmp_19 * m23 + tmp_20 * m33));
+    dst[10] =
+      d *
+      (tmp_14 * m03 +
+        tmp_19 * m13 +
+        tmp_22 * m33 -
+        (tmp_15 * m03 + tmp_18 * m13 + tmp_23 * m33));
+    dst[11] =
+      d *
+      (tmp_17 * m03 +
+        tmp_20 * m13 +
+        tmp_23 * m23 -
+        (tmp_16 * m03 + tmp_21 * m13 + tmp_22 * m23));
+    dst[12] =
+      d *
+      (tmp_14 * m22 +
+        tmp_17 * m32 +
+        tmp_13 * m12 -
+        (tmp_16 * m32 + tmp_12 * m12 + tmp_15 * m22));
+    dst[13] =
+      d *
+      (tmp_20 * m32 +
+        tmp_12 * m02 +
+        tmp_19 * m22 -
+        (tmp_18 * m22 + tmp_21 * m32 + tmp_13 * m02));
+    dst[14] =
+      d *
+      (tmp_18 * m12 +
+        tmp_23 * m32 +
+        tmp_15 * m02 -
+        (tmp_22 * m32 + tmp_14 * m02 + tmp_19 * m12));
+    dst[15] =
+      d *
+      (tmp_22 * m22 +
+        tmp_16 * m02 +
+        tmp_21 * m12 -
+        (tmp_20 * m12 + tmp_23 * m22 + tmp_17 * m02));
+
+    return new Matrix4(dst);
+  }
+
+  public determinant(): number {
+    const m = this.elements;
+
+    const m00 = m[0 * 4 + 0];
+    const m01 = m[0 * 4 + 1];
+    const m02 = m[0 * 4 + 2];
+    const m03 = m[0 * 4 + 3];
+    const m10 = m[1 * 4 + 0];
+    const m11 = m[1 * 4 + 1];
+    const m12 = m[1 * 4 + 2];
+    const m13 = m[1 * 4 + 3];
+    const m20 = m[2 * 4 + 0];
+    const m21 = m[2 * 4 + 1];
+    const m22 = m[2 * 4 + 2];
+    const m23 = m[2 * 4 + 3];
+    const m30 = m[3 * 4 + 0];
+    const m31 = m[3 * 4 + 1];
+    const m32 = m[3 * 4 + 2];
+    const m33 = m[3 * 4 + 3];
+    const tmp_0 = m22 * m33;
+    const tmp_1 = m32 * m23;
+    const tmp_2 = m12 * m33;
+    const tmp_3 = m32 * m13;
+    const tmp_4 = m12 * m23;
+    const tmp_5 = m22 * m13;
+    const tmp_6 = m02 * m33;
+    const tmp_7 = m32 * m03;
+    const tmp_8 = m02 * m23;
+    const tmp_9 = m22 * m03;
+    const tmp_10 = m02 * m13;
+    const tmp_11 = m12 * m03;
+
+    const t0 =
+      tmp_0 * m11 +
+      tmp_3 * m21 +
+      tmp_4 * m31 -
+      (tmp_1 * m11 + tmp_2 * m21 + tmp_5 * m31);
+    const t1 =
+      tmp_1 * m01 +
+      tmp_6 * m21 +
+      tmp_9 * m31 -
+      (tmp_0 * m01 + tmp_7 * m21 + tmp_8 * m31);
+    const t2 =
+      tmp_2 * m01 +
+      tmp_7 * m11 +
+      tmp_10 * m31 -
+      (tmp_3 * m01 + tmp_6 * m11 + tmp_11 * m31);
+    const t3 =
+      tmp_5 * m01 +
+      tmp_8 * m11 +
+      tmp_11 * m21 -
+      (tmp_4 * m01 + tmp_9 * m11 + tmp_10 * m21);
+
+    return 1.0 / (m00 * t0 + m10 * t1 + m20 * t2 + m30 * t3);
+  }
+
+  // these todo need to implement vector4 first
+
+  // todo transformVector if needed
+  // https://github.com/gfxfundamentals/webgl-fundamentals/blob/master/webgl/resources/m4.js#L1031
+
+  // todo transformPoint if needed
+  // https://github.com/gfxfundamentals/webgl-fundamentals/blob/master/webgl/resources/m4.js#L1031
+
+  // todo transformDirection if needed
+  // https://github.com/gfxfundamentals/webgl-fundamentals/blob/master/webgl/resources/m4.js#L1031
+
+  public transformNormal(v: Vector3): Vector3 {
+    const mi = this.inverse().elements;
+    const v0 = v.getComponent(0);
+    const v1 = v.getComponent(1);
+    const v2 = v.getComponent(2);
+
+    return new Vector3(
+      v0 * mi[0] + v1 * mi[4] + v2 * mi[8],
+      v0 * mi[1] + v1 * mi[5] + v2 * mi[9],
+      v0 * mi[2] + v1 * mi[6] + v2 * mi[10]
+    );
   }
 }
---
+++ src/utils/math/transformation.ts
@@ -0,0 +1,253 @@
+import { Vector3 } from './vector3.ts';
+import { Matrix4 } from './matrix4.ts';
+
+export class Transformation {
+  /**
+   * Create a lookAt matrix
+   * This is a world matrix for a camera. In other worlds, it will transform
+   * from the origin to a place and orientation in the world. For a view
+   * matrix take the inverse of this
+   * @param cameraPosition
+   * @param target
+   * @param up
+   */
+  public static lookAt(
+    cameraPosition: Vector3,
+    target: Vector3,
+    up: Vector3
+  ): Matrix4 {
+    const zAxis = cameraPosition.subVector(target).normalize();
+    const xAxis = up.cross(zAxis).normalize();
+    const yAxis = zAxis.cross(xAxis).normalize();
+
+    // prettier-ignore
+    return new Matrix4([
+        xAxis.getComponent(0), yAxis.getComponent(0), zAxis.getComponent(0), cameraPosition.getComponent(0),
+        xAxis.getComponent(1), yAxis.getComponent(1), zAxis.getComponent(1), cameraPosition.getComponent(1),
+        xAxis.getComponent(2), yAxis.getComponent(2), zAxis.getComponent(2), cameraPosition.getComponent(2),
+        0, 0, 0, 1
+    ]);
+  }
+
+  /**
+   * Compute a 4x4 pespective transformation matrix given the angular height
+   * of the frustum, the aspect ratio, and the near and far clipping planes.
+   * The arguments define a frustum extending in the negative z direction. The given
+   * angle is the vertical angle of the frustum, and the horizontal angle is
+   * determined to produce the given aspect ratio. The arguments near and far are the
+   * distance to the near and far clipping planes. Note that near and far are not z coordinates,
+   * but rather they are distances along the negative z-axis. The matrix generated sends the
+   * viewing frustum to the unit box. We assume a unit box extending from -1 to 1 in the x and y
+   * dimensions and from -1 to 1 in the z dimension.
+   *
+   * @param fieldOfViewInRadians field of view in y axis
+   * @param aspect aspect of viewport (width / height)
+   * @param near near Z clipping plane
+   * @param far far far Z clipping plane
+   */
+  public static perspective(
+    fieldOfViewInRadians: number,
+    aspect: number,
+    near: number,
+    far: number
+  ): Matrix4 {
+    const f = Math.tan(Math.PI * 0.5 - 0.5 * fieldOfViewInRadians);
+    const rangeInv = 1.0 / (near - far);
+
+    // prettier-ignore
+    return new Matrix4([
+        f/aspect, 0, 0, 0,
+        0, f, 0, 0,
+        0, 0, (near+far)*rangeInv, near*far*rangeInv*2,
+        0, 0, -1, 0
+    ])
+  }
+
+  /**
+   * Computes a 4-by-4 orthographic projection matrix given the coordinates of the
+   * planes defining the axis-aligned, box-shaped viewing volume.  The matrix
+   * generated sends that box to the unit box.  Note that although left and right
+   * are x coordinates and bottom and top are y coordinates, near and far
+   * are not z coordinates, but rather they are distances along the negative
+   * z-axis.  We assume a unit box extending from -1 to 1 in the x and y
+   * dimensions and from -1 to 1 in the z dimension.
+   * @param {number} left The x coordinate of the left plane of the box.
+   * @param {number} right The x coordinate of the right plane of the box.
+   * @param {number} bottom The y coordinate of the bottom plane of the box.
+   * @param {number} top The y coordinate of the right plane of the box.
+   * @param {number} near The negative z coordinate of the near plane of the box.
+   * @param {number} far The negative z coordinate of the far plane of the box.
+   */
+  public static orthographic(
+    left: number,
+    right: number,
+    bottom: number,
+    top: number,
+    near: number,
+    far: number
+  ): Matrix4 {
+    // prettier-ignore
+    return new Matrix4([
+        2/(right-left), 0, 0, (left+right) / (left-right),
+        0, 2/(top-bottom), 0, (bottom+top) / (bottom-top),
+        0, 0, 2/(near-far), (near+far) / (near-far),
+        0, 0, 0, 1
+    ])
+  }
+
+  /**
+   * Computes a 4-by-4 perspective transformation matrix given the left, right,
+   * top, bottom, near and far clipping planes. The arguments define a frustum
+   * extending in the negative z direction. The arguments near and far are the
+   * distances to the near and far clipping planes. Note that near and far are not
+   * z coordinates, but rather they are distances along the negative z-axis. The
+   * matrix generated sends the viewing frustum to the unit box. We assume a unit
+   * box extending from -1 to 1 in the x and y dimensions and from -1 to 1 in the z
+   * dimension.
+   * @param {number} left The x coordinate of the left plane of the box.
+   * @param {number} right The x coordinate of the right plane of the box.
+   * @param {number} bottom The y coordinate of the bottom plane of the box.
+   * @param {number} top The y coordinate of the right plane of the box.
+   * @param {number} near The negative z coordinate of the near plane of the box.
+   * @param {number} far The negative z coordinate of the far plane of the box.
+   */
+  public static frustum(
+    left: number,
+    right: number,
+    bottom: number,
+    top: number,
+    near: number,
+    far: number
+  ): Matrix4 {
+    const dx = right - left;
+    const dy = top - bottom;
+    const dz = far - near;
+
+    // prettier-ignore
+    return new Matrix4([
+        2*near/dx, 0, (left+right)/dx, 0,
+        0, 2*near/dy, (top+bottom)/dy, 0,
+        0, 0, -(far+near)/dz, -2*near*far/dz,
+        0, 0, -1, 0
+    ])
+  }
+
+  /**
+   * Makes a translation matrix
+   * @param {number} tx x translation.
+   * @param {number} ty y translation.
+   * @param {number} tz z translation.
+   */
+  public static translation(tx: number, ty: number, tz: number): Matrix4 {
+    // prettier-ignore
+    return new Matrix4([
+        1, 0, 0, tx,
+        0, 1, 0, ty,
+        0, 0, 1, tz,
+        0, 0, 0, 1
+    ]);
+  }
+
+  /**
+   * Make an x rotation matrix
+   * @param angleInRadians amount to rotate
+   */
+  public static xRotation(angleInRadians: number): Matrix4 {
+    const c = Math.cos(angleInRadians);
+    const s = Math.sin(angleInRadians);
+
+    // prettier-ignore
+    return new Matrix4([
+        1, 0, 0, 0,
+        0, c, -s, 0,
+        0, s, c, 0,
+        0, 0, 0, 1
+    ]);
+  }
+
+  /**
+   * Make an y rotation matrix
+   * @param angleInRadians amount to rotate
+   */
+  public static yRotation(angleInRadians: number): Matrix4 {
+    const c = Math.cos(angleInRadians);
+    const s = Math.sin(angleInRadians);
+
+    // prettier-ignore
+    return new Matrix4([
+        c, 0, s, 0,
+        0, 1, 0, 0,
+        -s, 0, c, 0,
+        0, 0, 0, 1
+    ]);
+  }
+
+  /**
+   * Make an z rotation matrix
+   * @param angleInRadians amount to rotate
+   */
+  public static zRotation(angleInRadians: number): Matrix4 {
+    const c = Math.cos(angleInRadians);
+    const s = Math.sin(angleInRadians);
+
+    // prettier-ignore
+    return new Matrix4([
+        c, -s,  0, 0,
+        s,  c,  0, 0,
+        0,  0,  1, 0,
+        0,  0,  0, 1
+    ]);
+  }
+
+  /**
+   * Makes an rotation matrix around an arbitrary axis
+   * @param axis axis to rotate around
+   * @param angleInRadians amount to rotate
+   */
+  public static axisRotation(axis: Vector3, angleInRadians: number): Matrix4 {
+    const norm = axis.normalize();
+    const x = norm.getComponent(0);
+    const y = norm.getComponent(1);
+    const z = norm.getComponent(2);
+
+    const xx = x * x;
+    const yy = y * y;
+    const zz = z * z;
+    const c = Math.cos(angleInRadians);
+    const s = Math.sin(angleInRadians);
+    const omc = 1 - c;
+
+    // prettier-ignore
+    return new Matrix4([
+        xx+(1-xx)*c, x*y*omc-z*s, x*z*omc+y*s, 0,
+        x*y*omc+z*s, yy+(1-yy)*c, y*z*omc-x*s, 0,
+        x*z*omc-y*s, y*z*omc+x*s, zz+(1-zz)*c, 0,
+        0,           0,           0, 1,
+    ])
+  }
+
+  /**
+   * Makes a scale matrix
+   * @param sx x scale.
+   * @param sy y scale.
+   * @param sz z scale.
+   */
+  public static scaling(sx: number, sy: number, sz: number): Matrix4 {
+    // prettier-ignore
+    return new Matrix4([
+        sx,  0,  0,  0,
+         0, sy,  0,  0,
+         0,  0, sz,  0,
+         0,  0,  0,  1,
+    ])
+  }
+
+  // todo compose function
+  // https://github.com/gfxfundamentals/webgl-fundamentals/blob/master/webgl/resources/m4.js#L1031
+
+  // todo quat from rotation matrix
+  // https://github.com/gfxfundamentals/webgl-fundamentals/blob/master/webgl/resources/m4.js#L1031
+
+  // todo decompose
+  // https://github.com/gfxfundamentals/webgl-fundamentals/blob/master/webgl/resources/m4.js#L1031
+}
