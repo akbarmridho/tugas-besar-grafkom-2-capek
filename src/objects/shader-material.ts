296d0fa4ac935badb3e4268eb8a75e30af43cb15
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2024-05-14 18:41:37 +0700

    feat: add new class ShaderMaterial

--- src/objects/shader-material.ts
+++ src/objects/shader-material.ts
@@ -1,5 +1,15 @@
 import { Serializable } from './serializable.ts';
 
-export class ShaderMaterial extends Serializable {
-  //
+export class ShaderMaterial {
+  /* Attribute */
+  private _vertexShader: String
+  private _fragmentShader: String
+  private _uniforms = {}
+
+  /* Constructor */
+  constructor(vertexShader: String = '', fragmentShader: String = '', uniforms = {}) {
+    this._vertexShader = vertexShader;
+    this._fragmentShader = fragmentShader;
+    this._uniforms = uniforms;
+  }
 }
