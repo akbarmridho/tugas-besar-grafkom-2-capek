6c2078c1aeb773c7f919f8b953fefe61ba1f7ae7
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2024-05-14 19:26:21 +0700

    feat: add new class Color

---
+++ src/objects/color.ts
@@ -0,0 +1,40 @@
+import { Serializable } from "./serializable";
+
+export interface ColorSerialized {
+    r: number;
+    g: number;
+    b: number;
+}
+
+export class Color extends Serializable<ColorSerialized> {
+    /* Attribute */
+    private r: number;
+    private g: number;
+    private b: number;
+
+    /* Constructor */
+    constructor(r: number, g: number, b: number) {
+        this.r = r;
+        this.g = g;
+        this.b = b;
+    }
+
+    clone(): Color {
+        return new Color(this.r, this.g, this.b);
+    }
+
+    copy(otherColor: Color): Color {
+        this.r = otherColor.r;
+        this.g = otherColor.g;
+        this.b = otherColor.b;
+        return this;
+    }
+
+    toJSON(): ColorSerialized {
+        return {
+            r: this.r,
+            g: this.g,
+            b: this.b
+        };
+    }
+}
