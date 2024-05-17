cf99751de6077b73f27e87edd6ed2ed232cf5482
yobeldc <13521067@std.stei.itb.ac.id>
2024-05-17 16:26:32 +0700

    feat: add ArrayCamera class for creating array cameras

---
+++ src/objects/camera/array-camera.ts
@@ -0,0 +1,14 @@
+// import { PerspectiveCamera } from './perspective-camera';
+
+// export class ArrayCamera extends PerspectiveCamera {
+//     cameras: PerspectiveCamera[];
+//     isArrayCamera: boolean; // Add the isArrayCamera property
+
+//     constructor(array: PerspectiveCamera[] = []) {
+//         super();
+//         this.isArrayCamera = true;
+//         this.cameras = array;
+//     }
+// }
+
+
