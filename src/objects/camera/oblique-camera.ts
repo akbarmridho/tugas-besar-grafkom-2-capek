79574a169afd41c87ea3664a7adce390894f6d97
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2024-05-22 17:55:10 +0700

    fix oblique camera

--- src/objects/camera/oblique-camera.ts
+++ src/objects/camera/oblique-camera.ts
@@ -6,11 +6,13 @@ import { Transformation } from '@/utils/math/transformation.ts';
 import { Matrix4 } from '@/utils/math/matrix4.ts';
 
 export interface ObliqueProjection {
-  fov: number;
-  aspect: number;
+  top: number;
+  bottom: number;
+  left: number;
+  right: number;
   near: number;
   far: number;
-  theta: number; // Angle of obliqueness in radians
+  alpha: number;
 }
 
 export interface ObliqueCameraSerialized extends NodeSerialized {
@@ -23,11 +25,13 @@ export class ObliqueCamera extends Camera<ObliqueCameraSerialized> {
   constructor(
     name: string,
     projection: ObliqueProjection = {
-      fov: 75,
-      aspect: 16 / 9,
+      top: 1,
+      bottom: -1,
+      left: -1,
+      right: 1,
       near: 0.1,
-      far: 1000,
-      theta: 0.0 // Default angle of obliqueness
+      far: 2000,
+      alpha: Math.PI / 4
     },
     position?: Vector3,
     rotation?: Euler,
@@ -47,51 +51,61 @@ export class ObliqueCamera extends Camera<ObliqueCameraSerialized> {
     this.computeProjectionMatrix();
   }
 
-computeProjectionMatrix() {
-    const fov = this._baseProjection.fov;
-    const aspect = this._baseProjection.aspect;
-    const near = this._baseProjection.near;
-    const far = this._baseProjection.far;
-    const theta = this._baseProjection.theta;
+  computeProjectionMatrix() {
+    const dx =
+      (this._baseProjection.right - this._baseProjection.left) /
+      (2 * this.zoom);
+    const dy =
+      (this._baseProjection.top - this._baseProjection.bottom) /
+      (2 * this.zoom);
+    const cx = (this._baseProjection.right + this._baseProjection.left) / 2;
+    const cy = (this._baseProjection.top + this._baseProjection.bottom) / 2;
 
-    const tanHalfFov = Math.tan(fov / 2);
-    const top = near * tanHalfFov;
-    const bottom = -top;
-    const right = top * aspect;
-    const left = -right;
+    const left = cx - dx;
+    const right = cx + dx;
+    const top = cy + dy;
+    const bottom = cy - dy;
 
-    const projectionMatrix = new Matrix4([
-        (2 * near) / (right - left), 0, 0, 0,
-        0, (2 * near) / (top - bottom), 0, 0,
-        (right + left) / (right - left), (top + bottom) / (top - bottom), -1, -1,
-        0, 0, (-2 * near * far) / (far - near), 0
-    ]);
-
-    // Apply oblique projection transformation
-    this.projectionMatrix = this.computeObliqueProjectionMatrix(
-        projectionMatrix,
-        theta
+    const projection = Transformation.orthographic(
+      left,
+      right,
+      bottom,
+      top,
+      this._baseProjection.near,
+      this._baseProjection.far
     );
-}      
 
-  private computeObliqueProjectionMatrix(matrix: Matrix4, theta: number): Matrix4 {
-    // Compute the oblique projection matrix transformation
-    const near = this._baseProjection.near;
-    const far = this._baseProjection.far;
-    const tanTheta = Math.tan(theta);
+    // shear matrix
+    // ref: http://www.flipcode.com/documents/matrfaq.html#Q43
+    //        | 1    Syx  Szx  0 |
+    //        |                  |
+    //        | Sxy  1    Szy  0 |
+    //    M = |                  |
+    //        | Sxz  Syz  1    0 |
+    //        |                  |
+    //        | 0    0    0    1 |
+    //        |                  |
+    const alpha = Math.PI / 6; // or Math.PI / 4
 
-    const c = (far + near) / (far - near);
-    const d = (2 * far * near) / (far - near);
+    const Syx = 0,
+      Szx = -0.5 * Math.cos(alpha),
+      Sxy = 0,
+      Szy = -0.5 * Math.sin(alpha),
+      Sxz = 0,
+      Syz = 0;
 
-    const obliqueMatrix = new Matrix4([
-      1, 0, 0, 0,
-      0, 1, 0, 0,
-      tanTheta, 0, 1, 0,
+    // note:
+    // we translate x by -0.4 and y by -0.25
+    // in order to make the object centered to canvas
+    // prettier-ignore
+    const shear = new Matrix4([
+      1, Syx, Szx, -0.4,
+      Sxy, 1, Szy, -0.25,
+      Sxz, Syz, 1, 0,
       0, 0, 0, 1
     ]);
 
-    // Apply the oblique projection transformation to the standard perspective projection matrix
-    return matrix.multiplyMatrix(obliqueMatrix);
+    this.projectionMatrix = projection.preMultiplyMatrix(shear);
   }
 
   public toJSON(): ObliqueCameraSerialized {
