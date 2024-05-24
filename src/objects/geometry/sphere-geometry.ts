91eaa788c9ba088b960d9548ed98ea63016556b8
yobeldc <13521067@std.stei.itb.ac.id>
2024-05-24 17:25:02 +0700

    feat: add texcoord

--- src/objects/geometry/sphere-geometry.ts
+++ src/objects/geometry/sphere-geometry.ts
@@ -5,10 +5,10 @@ export interface SphereGeometryProps {
     radius: number;
     widthSegments: number;
     heightSegments: number;
-    phiStart: number;
-    phiLength: number;
-    thetaStart: number;
-    thetaLength: number;
+    phiStart?: number;
+    phiLength?: number;
+    thetaStart?: number;
+    thetaLength?: number;
 }
 
 export type SphereGeometrySerialized = BufferGeometrySerialized & SphereGeometryProps;
@@ -34,6 +34,7 @@ export class SphereGeometry extends BufferGeometry<SphereGeometrySerialized> {
         super({
             position: new BufferAttribute(new Float32Array(0), 3),
             normal: new BufferAttribute(new Float32Array(0), 3),
+            texcoord: new BufferAttribute(new Float32Array(0), 2),
         });
 
         this.radius = radius;
@@ -49,10 +50,11 @@ export class SphereGeometry extends BufferGeometry<SphereGeometrySerialized> {
 
         const vertices: number[] = [];
         const normals: number[] = [];
+        const uvs: number[] = [];
 
         let index = 0;
 
-        // Generate vertices and normals
+        // Generate vertices, normals, and uvs
         for (let iy = 0; iy <= this.heightSegments; iy++) {
             const verticesRow: number[] = [];
             const v = iy / this.heightSegments;
@@ -72,6 +74,8 @@ export class SphereGeometry extends BufferGeometry<SphereGeometrySerialized> {
 
                 normals.push(nx, ny, nz);
 
+                uvs.push(u, 1 - v); // Flip V coordinate
+
                 verticesRow.push(index++);
             }
 
@@ -81,6 +85,7 @@ export class SphereGeometry extends BufferGeometry<SphereGeometrySerialized> {
         // Generate triangles
         const triangles: number[] = [];
         const triangleNormals: number[] = [];
+        const triangleUVs: number[] = [];
         for (let iy = 0; iy < this.heightSegments; iy++) {
             for (let ix = 0; ix < this.widthSegments; ix++) {
                 const a = grid[iy][ix + 1];
@@ -99,6 +104,11 @@ export class SphereGeometry extends BufferGeometry<SphereGeometrySerialized> {
                         normals[b * 3], normals[b * 3 + 1], normals[b * 3 + 2],
                         normals[d * 3], normals[d * 3 + 1], normals[d * 3 + 2]
                     );
+                    triangleUVs.push(
+                        uvs[a * 2], uvs[a * 2 + 1],
+                        uvs[b * 2], uvs[b * 2 + 1],
+                        uvs[d * 2], uvs[d * 2 + 1]
+                    );
                 }
                 if (iy !== this.heightSegments - 1 || thetaEnd < Math.PI) {
                     triangles.push(
@@ -111,12 +121,18 @@ export class SphereGeometry extends BufferGeometry<SphereGeometrySerialized> {
                         normals[c * 3], normals[c * 3 + 1], normals[c * 3 + 2],
                         normals[d * 3], normals[d * 3 + 1], normals[d * 3 + 2]
                     );
+                    triangleUVs.push(
+                        uvs[b * 2], uvs[b * 2 + 1],
+                        uvs[c * 2], uvs[c * 2 + 1],
+                        uvs[d * 2], uvs[d * 2 + 1]
+                    );
                 }
             }
         }
 
         this.setAttribute('position', new BufferAttribute(new Float32Array(triangles), 3));
         this.setAttribute('normal', new BufferAttribute(new Float32Array(triangleNormals), 3));
+        this.setAttribute('texcoord', new BufferAttribute(new Float32Array(triangleUVs), 2));
     }
 
     toJSON(withNodeAttributes: boolean = true): SphereGeometrySerialized {
@@ -131,6 +147,8 @@ export class SphereGeometry extends BufferGeometry<SphereGeometrySerialized> {
             attributes: {
                 position: this.attributes.position.toJSON(),
                 normal: this.attributes.normal.toJSON(),
+                // @ts-ignore
+                texcoord: this.attributes.texcoord.toJSON(),
             },
         } as SphereGeometrySerialized;
 
