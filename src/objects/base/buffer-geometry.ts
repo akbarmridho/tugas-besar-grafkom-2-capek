737e64715a8d20f3a11aac195c3b7b91a3629866
GoDillonAudris512 <110383663+GoDillonAudris512@users.noreply.github.com>
2024-05-25 14:47:21 +0700

    feat: compute tangent and bitangent vector for geometry

--- src/objects/base/buffer-geometry.ts
+++ src/objects/base/buffer-geometry.ts
@@ -1,3 +1,4 @@
+import { Vector2 } from '@/utils/math/vector2.ts';
 import {
   BufferAttribute,
   BufferAttributeSerialized
@@ -10,6 +11,8 @@ export interface BaseBufferGeometryAttribute extends AttributeObject {
   position: BufferAttribute;
   normal: BufferAttribute;
   texcoord?: BufferAttribute;
+  tangent: BufferAttribute;
+  bitangent: BufferAttribute;
 }
 
 export interface BufferGeometrySerialized {
@@ -17,6 +20,8 @@ export interface BufferGeometrySerialized {
     position: BufferAttributeSerialized;
     normal?: BufferAttributeSerialized;
     texcoord?: BufferAttributeSerialized;
+    tangent?: BufferAttribute;
+    bitangent?: BufferAttribute;
   } & Partial<{ [attr: string]: BufferAttributeSerialized }>;
 }
 
@@ -49,10 +54,30 @@ export abstract class BufferGeometry<
 
     this._computeNormal(attributes.position, normal);
 
+    let tangent = new BufferAttribute(
+      new Float32Array(attributes.position.length),
+      attributes.position.size
+    );
+    let bitangent = new BufferAttribute(
+      new Float32Array(attributes.position.length),
+      attributes.position.size
+    );
+
+    if (attributes.texcoord) {
+      this._computeTangentBitangent(
+        attributes.position,
+        attributes.texcoord,
+        tangent,
+        bitangent
+      );
+    }
+
     this._attributes = {
       position: attributes.position,
       texcoord: attributes.texcoord,
-      normal: normal
+      normal: normal,
+      tangent: tangent,
+      bitangent: bitangent
     };
   }
 
@@ -113,6 +138,72 @@ export abstract class BufferGeometry<
       normal.set(i, n.toArray());
       normal.set(i + 1, n.toArray());
       normal.set(i + 2, n.toArray());
+    }
+  }
+
+  _computeTangentBitangent(
+    position: BufferAttribute,
+    texcoord: BufferAttribute,
+    tangent: BufferAttribute,
+    bitangent: BufferAttribute
+  ) {
+    const tangentVec = new Vector3();
+    const bitangentVec = new Vector3();
+
+    const edge1 = new Vector3();
+    const edge2 = new Vector3();
+    const deltaUV1 = new Vector2();
+    const deltaUV2 = new Vector2();
+
+    for (let i = 0; i < position.count; i += 3) {
+      const p1 = position.get(i);
+      const p2 = position.get(i + 1);
+      const p3 = position.get(i + 2);
+
+      const uv1 = texcoord.get(i);
+      const uv2 = texcoord.get(i + 1);
+      const uv3 = texcoord.get(i + 2);
+
+      edge1.fromArray([p2[0] - p1[0], p2[1] - p1[1], p2[2] - p1[2]]);
+      edge2.fromArray([p3[0] - p1[0], p3[1] - p1[1], p3[2] - p1[2]]);
+
+      deltaUV1.fromArray([uv2[0] - uv1[0], uv2[1] - uv1[1]]);
+      deltaUV2.fromArray([uv3[0] - uv1[0], uv3[1] - uv1[1]]);
+
+      // prettier-ignore
+      const f = 1.0 / (deltaUV1.getComponent(0) * deltaUV2.getComponent(1) - deltaUV2.getComponent(0) * deltaUV1.getComponent(1));
+
+      tangentVec.setComponents(
+        f *
+          (deltaUV2.getComponent(1) * edge1.getComponent(0) -
+            deltaUV1.getComponent(1) * edge2.getComponent(0)),
+        f *
+          (deltaUV2.getComponent(1) * edge1.getComponent(1) -
+            deltaUV1.getComponent(1) * edge2.getComponent(1)),
+        f *
+          (deltaUV2.getComponent(1) * edge1.getComponent(2) -
+            deltaUV1.getComponent(1) * edge2.getComponent(2))
+      );
+
+      bitangentVec.setComponents(
+        f *
+          (deltaUV2.getComponent(0) * edge1.getComponent(0) +
+            deltaUV1.getComponent(0) * edge2.getComponent(0)),
+        f *
+          (deltaUV2.getComponent(0) * edge1.getComponent(1) +
+            deltaUV1.getComponent(0) * edge2.getComponent(1)),
+        f *
+          (deltaUV2.getComponent(0) * edge1.getComponent(2) +
+            deltaUV1.getComponent(0) * edge2.getComponent(2))
+      );
+
+      tangent.set(i, tangentVec.toArray());
+      tangent.set(i + 1, tangentVec.toArray());
+      tangent.set(i + 2, tangentVec.toArray());
+
+      bitangent.set(i, bitangentVec.toArray());
+      bitangent.set(i + 1, bitangentVec.toArray());
+      bitangent.set(i + 2, bitangentVec.toArray());
     }
   }
 }
